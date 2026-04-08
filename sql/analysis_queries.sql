11bd4c6b08d5fd66e10c65283886e381bd2920c8
Shyamkrishna Sreeramasetty <sshyamkrishna67@gmail.com>
2026-04-08 09:27:35 +0100

    Update analysis_queries.sql

--- sql/analysis_queries.sql
+++ sql/analysis_queries.sql
@@ -13,3 +13,17 @@ GROUP BY sub_category;
 SELECT order_year, SUM(sales) AS yearly_sales
 FROM sales
 GROUP BY order_year;
+
+-- Top 5 products by profit
+SELECT product_name, SUM(profit) AS total_profit
+FROM sales
+GROUP BY product_name
+ORDER BY total_profit DESC
+LIMIT 5;
+
+-- Loss making products
+SELECT product_name, SUM(profit) AS total_profit
+FROM sales
+GROUP BY product_name
+ORDER BY total_profit ASC
+LIMIT 5;
